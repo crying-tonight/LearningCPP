1d81d84acb2c335bc665b41e52b940e989955d61
crying-tonight <157319255+crying-tonight@users.noreply.github.com>
2024-04-18 18:27:04 -0300

    done

---
+++ Calculator/First.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+
+class variables{
+    public:
+        string x;
+
+};
+
+
+void menue();
+/*string scanf(string input);
+void printf(string input);*/
+
+void fget(string input);
+
+int main(){
+    printf("hello");
+
+}
+
+void menue(){
+    switc
+}
+
+/*void printf(string input){
+    cout << input;
+}
+
+string scanf(string input){
+    cout << input;
+    string output = cin >> x;
+    return output;
+}*/
